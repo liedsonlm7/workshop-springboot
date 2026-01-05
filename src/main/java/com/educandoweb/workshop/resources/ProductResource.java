cb3367b762aeb5b319fff92dac9479fdccb08319
liedsonlm7 <liedsonlm7@gmail.com>
2026-01-05 18:25:40 -0300

    (feat): create entity Product

---
+++ src/main/java/com/educandoweb/workshop/resources/ProductResource.java
@@ -0,0 +1,32 @@
+package com.educandoweb.workshop.resources;
+
+import com.educandoweb.workshop.entities.Product;
+import com.educandoweb.workshop.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> Products = service.findAll();
+        return ResponseEntity.ok().body(Products);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product Product = service.findById(id);
+        return ResponseEntity.ok().body(Product);
+    }
+}
