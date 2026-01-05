8023d96cedf0370bb2184d303fd7064b8db0ebd9
liedsonlm7 <liedsonlm7@gmail.com>
2026-01-05 17:59:37 -0300

    (feat): create entity Category

---
+++ src/main/java/com/educandoweb/workshop/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.educandoweb.workshop.repositories;
+
+import com.educandoweb.workshop.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
