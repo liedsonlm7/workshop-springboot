9a68820b3fa55f2e25d511fae056cb53b66d114c
liedsonlm7 <liedsonlm7@gmail.com>
2026-01-06 17:19:44 -0300

    (feat): Payment, one-to-one association

--- src/main/java/com/educandoweb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -63,8 +63,6 @@ public class TestConfig implements CommandLineRunner {
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 
-
-
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
@@ -74,5 +72,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
---
+++ src/main/java/com/educandoweb/workshop/entities/Payment.java
@@ -0,0 +1,54 @@
+package com.educandoweb.workshop.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {}
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Payment payment)) return false;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
